cc55a8388b600f27e120886ab49f783f89e2189d
Yolanda <alumoy1404@ieselcaminas.org>
2021-11-08 16:39:51 +0100

    Ejercicio1

---
+++ src/PracticaPuntuable01.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class PracticaPuntuable01 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        //Declaracion de variables
+
+        int[] numeros = new int[7];
+        int n;
+        int n1 = 2;
+        int n2 = 7;
+        int n3 = 4;
+        int n4 = -5;
+        int n5 = 11;
+        int n6 = 5;
+        int n7 = 20;
+        boolean == true;
+        int suma;
+
+
+        //Lectura de datos
+
+        System.out.println("Introduce los numeros 2, 7, 4, -5, 11, 5, 20: ");
+
+
+        for (int i = 0; i < numeros.length; i++) {                                 //Iterador recorre el array
+            n = sc.nextInt();
+
+              boolean == false;
+            for (int j = (i-1); j < numeros.length; j++) {                         //Iterador i compara todos los valores del array
+                if(n1 + n2 ) {
+
+
+                }
+
+            }
+        }
+
+        suma = suma + 15;                                                          //Suma de valores para que el resultado sea 15
+    }
+}
